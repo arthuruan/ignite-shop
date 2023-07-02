276f88a2a1b7600fee03a87fa489c05573948702
Arthur Ruan <arthur123ruan@hotmail.com>
2023-07-02 20:58:33 -0300

    - Added product style - Added product content

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,28 @@
+import {
+    ImageContainer,
+    ProductContainer,
+    ProductDetails,
+} from '@/styles/pages/product'
 import { useRouter } from 'next/router'
 
 export default function Product() {
     const { query } = useRouter()
-    return <h1>Product</h1>
+    return (
+        <ProductContainer>
+            <ImageContainer></ImageContainer>
+
+            <ProductDetails>
+                <h1>Camiseta x</h1>
+                <span>R$ 79,90</span>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Ratione, iure quaerat fugit deleniti excepturi eos
+                    necessitatibus quae, ea quidem eveniet expedita aliquam
+                    voluptate earum eum voluptatum assumenda libero minima
+                    consectetur?
+                </p>
+                <button>Comprar agora</button>
+            </ProductDetails>
+        </ProductContainer>
+    )
 }
