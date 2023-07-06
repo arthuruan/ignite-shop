f91637a4bb7cb483163bee302f751b9e36ff36f5
Arthur Ruan <arthur123ruan@hotmail.com>
2023-07-05 22:31:02 -0300

    - Added checkout api integration

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,7 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
 import Image from 'next/image'
 import Stripe from 'stripe'
+import axios from 'axios'
 
 import { stripe } from '@/lib/stripe'
 import {
@@ -8,6 +9,7 @@ import {
     ProductContainer,
     ProductDetails,
 } from '@/styles/pages/product'
+import { useState } from 'react'
 
 interface ProductProps {
     product: {
@@ -21,8 +23,24 @@ interface ProductProps {
 }
 
 export default function Product({ product }: ProductProps) {
+    const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] =
+        useState(false)
+
     async function handleBuyProduct() {
-        console.log(product.defaultPriceId)
+        try {
+            setIsCreatingCheckoutSession(true)
+            const response = await axios.post('/api/checkout', {
+                priceId: product.defaultPriceId,
+            })
+
+            const { checkoutUrl } = response.data
+
+            window.location.href = checkoutUrl
+        } catch (err: any) {
+            // Connect with a tool that can observate the error (Datadog / Sentry)
+            setIsCreatingCheckoutSession(false)
+            alert('Falha ao redericionar ao checkout!')
+        }
     }
 
     return (
@@ -35,7 +53,12 @@ export default function Product({ product }: ProductProps) {
                 <h1>{product.name}</h1>
                 <span>{product.price}</span>
                 <p>{product.description}</p>
-                <button onClick={handleBuyProduct}>Comprar agora</button>
+                <button
+                    disabled={isCreatingCheckoutSession}
+                    onClick={handleBuyProduct}
+                >
+                    Comprar agora
+                </button>
             </ProductDetails>
         </ProductContainer>
     )
